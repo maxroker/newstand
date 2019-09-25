ba524923445489b4c18c637c12f0067c01dd0fbc
dankomaksym <dankomaksym@gmail.com>
2019-09-25 22:48:58 +0300

    Add simple_format and 1 more article

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,33 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Article.create(title: "Is there someone who can watch us?", author: "Matt Healy", body: "I used to have a reoccurring dream when I was younger. The narrative was loosely based, but the setting was always the surrounding areas of where my parents grew up. I think I was slightly obsessed with how certain, bleak areas of Northern England contained my history and the soul of my family, my psyche - I always felt connected and disconnected at the same time. Metaphorically and physically I would visit these places frequently as a youngster and in turn have used them as the setting to a lot of the stories in The 1975 - the North East was the visual setting to my formative years.")
-Article.create(title: "Between the stripes", author: "Leonardo San Miguel", body: 'Pantomime lore dates as far back as the eight century BC, but it was never formalized as an art—the one we know now we know now—until around the sixteenth century in Easter Europe. Although almost every record of Pantomime implies the art was primarily conceived as a form of entertainment, there have always been traces that indicate the art was a manifestation of a grander concept: that of revelation through absence. Indeed, a pantomime’s performance is, however comical or banal, a form of illusionism. As author Edward Lancaster writes in The Mysticism of Performing Arts, “There is no greater demonstration of magic than that of creating entire worlds and realities through the use of nothing but one’s body in motion and the unrelenting desire of the human mind to fill in the blanks.”
-')
-Article.create(title: "Water", author: "Diana Storm", body: "Benjamin is laying on a field of grass and it is the middle of a windy, but warm day. He is staring at the sky. Blue looking at blue, but hidden under a pair of aviators. His hair cannot yet decide if it is a dark yellow or a light brown, and he likes that.")
+#Article.create(title: "Is there someone who can watch us?", author: "Matt Healy", body: "I used to have a reoccurring dream when I was younger. The narrative was loosely based, but the setting was always the surrounding areas of where my parents grew up. I think I was slightly obsessed with how certain, bleak areas of Northern England contained my history and the soul of my family, my psyche - I always felt connected and disconnected at the same time. Metaphorically and physically I would visit these places frequently as a youngster and in turn have used them as the setting to a lot of the stories in The 1975 - the North East was the visual setting to my formative years.")
+#Article.create(title: "Between the stripes", author: "Leonardo San Miguel", body: 'Pantomime lore dates as far back as the eight century BC, but it was never formalized as an art—the one we know now we know now—until around the sixteenth century in Easter Europe. Although almost every record of Pantomime implies the art was primarily conceived as a form of entertainment, there have always been traces that indicate the art was a manifestation of a grander concept: that of revelation through absence. Indeed, a pantomime’s performance is, however comical or banal, a form of illusionism. As author Edward Lancaster writes in The Mysticism of Performing Arts, “There is no greater demonstration of magic than that of creating entire worlds and realities through the use of nothing but one’s body in motion and the unrelenting desire of the human mind to fill in the blanks.”
+#')
+#Article.create(title: "Water", author: "Diana Storm", body: "Benjamin is laying on a field of grass and it is the middle of a windy, but warm day. He is staring at the sky. Blue looking at blue, but hidden under a pair of aviators. His hair cannot yet decide if it is a dark yellow or a light brown, and he likes that.")
+Article.create(title: "25 Toxic Things You Need To Let Go Of By 25", author: "Kendra Syrdal", body: "
+	 1. People who make you question yourself, your self-worth, and who you are as a human being.
+	 2. The fear of dying alone, never finding love, or not being enough for someone.
+	 3. Any sense that you “deserve” to have something that you aren’t willing to work for.
+	 4. The people who didn’t love you back, and especially the ones who never will.
+	 5. An unwillingness to work to get better at something since you’re not in college and a grade doesn’t depend on it.
+	 6. The idea that asking for help makes you weak.
+	 7. Considering yourself “not like other girls” and therefore, writing off girls as friends.
+	 8. Considering yourself “not like most guys” and therefore, writing off guys as friends.
+	 9. Writing off ANYONE as a potential friend before you’ve actually taken the time to get to know them.
+	 10. Any anxiety of things that are new to you just because they aren’t like what you’ve always known.
+	 11. Prejudices that have been established because of the unknown.
+	 12. Habits that are bad enough that they make you think about breaking them in the first place.
+	 13. Being uncomfortable with sex and talking about it so much that it affects your ability to have a good time when you’re having it.
+	 14. The idea that there’s only one person out there for you.
+	 15. Places, people, and situations that you’ve ultimately outgrown.
+	 16. The glorification of being a complete disaster, when you could be getting your life to a place you’re proud of.
+	 17. Any labels that are on you that you don’t 100% identify with.
+	 18. The thought that life is going to be fair, and even more, that you deserve it to be.
+	 19. Putting so much pressure on yourself that you’re constantly stressed and tense.
+	 20.Never letting yourself take a break, or stop, or breathe.
+	 21. Putting pressure on yourself to look a certain way, instead of loving the body that you have.
+	 22. The idea that one person “should make the first move” when you could go after what you want if you just sucked it up.
+	 23. Your fear of failing, because you’re going to and you’re going to be okay even after you do.
+	 24. Your need to be “perfect.” Because you aren’t and you’re never going to be.
+	 25. The idea that there’s one “right way” to be a 20-something.")
